fb8f90a6d29de72011444093edf9a03e165e5a25
MokkeMeguru <meguru.mokke@gmail.com>
2020-03-24 15:41:43 +0900

    update ui

--- src/main/jnmongo_labeler/frontend/view.cljs
+++ src/main/jnmongo_labeler/frontend/view.cljs
@@ -7,7 +7,10 @@
    [jnmongo-labeler.utils.parser :as parser]))
 
 (defn navbar []
-  [:nav#nav-box.navbar.box])
+  [:nav#nav-box.navbar.box
+   [:div.navbar-start>p "JNMongo Labeler"]
+   [:div.navbar-end [:a {:href "https://mokkemeguru.github.io/portfolio/"} "contact"]]
+   ])
 
 (defn workspace-title []
   (let [title (r/atom "")]
@@ -15,6 +18,7 @@
      [:div>h2 "title > " (if @(rf/subscribe [::subs/contents]) "エクス・アルビオ")]
      [:div
       [:input {:type "text"
+               :style {:font-size "1rem"}
                :on-key-down #(when (= 13 (.-keyCode js/window.event))
                                (rf/dispatch-sync [::events/load-static-file]))
                :on-change #(reset! title (-> % .-target .-value))
@@ -37,31 +41,20 @@
   (let [original section
         keywords (r/atom (set []))
         on-mouse-up-event (fn [] (when-let [[type selection] (getSelected)]
-                                  (swap! keywords conj (str selection))
-                                   ;; (condp = type
-                                   ;;   "get"
-                                   ;;   (let [range (.getRangeAt selection 0)
-                                   ;;         new-node
-                                   ;;         (doto
-                                   ;;             (js/document.createElement "span")
-                                   ;;           (#(set! (.-className %) "selectedText"))
-                                   ;;           (#(set! (.-innerHTML %) (str selection))))]
-                                   ;;     (doto range
-                                   ;;       .deleteContents
-                                   ;;       (.insertNode new-node)))
-                                   ;;   "range"
-                                   ;;   (println "var"))
-                                   ))
+                                  (swap! keywords conj (str selection))))
         keywords-list (fn []
-                        [:ul.box {:style {:max-width "200px"}}
-                         (map (fn [k]
-                                [:li [:div.columns
-                                      [:div.column.is-10>p.has-text-justified k]
-                                      [:div.column>button.button.is-info.is-light
-                                       {:on-click #(swap! keywords disj k)} "X"]]])
-                              @keywords)])]
+                        [:div.box {:style {:max-height "200px"
+                                            :overflow-y "auto"
+                                            :overflow-x "auto"}}
+                         [:ul
+                          (map (fn [k]
+                                 [:li [:div.columns.is-vcentered
+                                       [:div.column.is-9>p.has-text-justified k]
+                                       [:div.column.is-3>button.button.is-info.is-light.is-small
+                                        {:on-click #(swap! keywords disj k)} "X"]]])
+                               @keywords)]])]
     [:div.column>div.columns
-     [:div.column.is-four-fifths>div.card>div.card-content
+     [:div.column.is-9>div.card>div.card-content
        {:on-mouse-up on-mouse-up-event}
       (parser/parse section)]
      [:div.column
@@ -71,7 +64,7 @@
   [:div.columns>div.column.is-8.is-offset-2
    (workspace-title)
    [:div.columns
-    [:div.column.is-four-fifths>p "HTML viewer"]
+    [:div.column.is-four-fifths.has-text-centered>p "HTML viewer"]
     [:div.column>p "keywords viewer"]]
    (if-let [contents  @(rf/subscribe [::subs/contents])]
      (map #(view-section %) (:article contents)))])
